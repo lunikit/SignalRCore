8d7d2d7450325167407e93a35604a45da7ea2960
Nikita Luksha <nikita.luksha@gmail.com>
2018-01-21 14:55:22 +0300

    connect hub to api

--- Chater/Chater/Controllers/ChatController.cs
+++ Chater/Chater/Controllers/ChatController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
+using Chater.Hubs;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Chater.Controllers
 {
@@ -11,6 +9,13 @@ namespace Chater.Controllers
     [Route("api/Chat")]
     public class ChatController : Controller
     {
+        private readonly IHubContext<ChatHub> _context;
+
+        public ChatController(IHubContext<ChatHub> context)
+        {
+            _context = context;
+        }
+
         // GET: api/Chat
         [HttpGet]
         public IEnumerable<string> Get()
@@ -22,6 +27,8 @@ namespace Chater.Controllers
         [HttpGet("{message}", Name = "Get")]
         public string Get(string message)
         {
+            this._context.Clients.All.InvokeAsync("Send", message);
+
             return message;
         }
     }
