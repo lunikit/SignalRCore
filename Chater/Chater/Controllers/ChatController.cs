76ce9da7720d8cfb33e61d80d9090ae13bce129a
Nikita Luksha <nikita.luksha@gmail.com>
2018-01-21 14:32:35 +0300

    add api

---
+++ Chater/Chater/Controllers/ChatController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chater.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Chat")]
+    public class ChatController : Controller
+    {
+        // GET: api/Chat
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Chat/5
+        [HttpGet("{message}", Name = "Get")]
+        public string Get(string message)
+        {
+            return message;
+        }
+    }
+}
